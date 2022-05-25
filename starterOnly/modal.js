79ce5b330b3b4618e8bad5fed520237fe3d51dc5
github_ayns789 <joly.a.idf@gmail.com>
2022-05-25 20:42:03 +0200

    up check functions for integrate datas saved in session storage

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -50,9 +50,12 @@ const checkboxVal1 = document.getElementById("checkbox1");
 const checkboxVal2 = document.getElementById("checkbox2");
 const errorCheckbox = document.getElementById("ckeckboxError");
 
-// regex
-const regexLetters = /^[a-zA-Z-\s]+$/;
+///////////// regex //////////////
+// check letters 
+const regexLetters = /^[ a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`'\-]+$/;
+// check email format
 const regexMail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,}$/;
+// date checked true format between 1930 to 2009
 const regexBirthdate = /^((19[3-9]+[0-9]|200[0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])|(0?[1-9]|[12]\d|3[01])[/](0?[1-9]|1[0-2])[/](19[3-9]+[0-9]|200[0-6]))$/;
 
 //////////////////////////////
@@ -68,14 +71,33 @@ closeModal.addEventListener('click', function() {
 });
 /////////////////////////////////////////////
 
+// check navigator can use session storage 
+// if(sessionStorage == null) {
+  //   console.log("Storage non supporté par le navigateur");
+  // }
+
+
+////////////////////////////////////////////
 // get firstName user
-firstName.addEventListener('change', checkFirstName);
-// check firstName user
+firstName.addEventListener('input', checkFirstName);
+
 let firstNameOk = false;
 let valFirstName = "";
 
+// check firstname saved in session storage
+if (sessionStorage.firstnameSaved) {
+  firstName.value = sessionStorage.firstnameSaved;
+} 
+
 function checkFirstName(e){
-  let valFirstName = e.target.value;
+
+  if (sessionStorage.firstnameSaved) {
+    valFirstName = e.newValue;
+  } 
+
+  valFirstName = e.target.value;
+  sessionStorage.setItem("firstnameSaved", valFirstName);
+
   if( valFirstName.length < 2 ){
     errorFirstName.innerText = "Vous devez saisir au moins 2 caractères";
     errorFirstName.style.color = "red";
@@ -95,13 +117,22 @@ e.preventDefault();
 ///////////////////////////////////////////
 
 // check lastName user
-lastName.addEventListener('change', checkLastName);
+lastName.addEventListener('input', checkLastName);
 
 let lastNameOk = false;
 let valLastName = "";
 
+if (sessionStorage.lastnameSaved) {
+  lastName.value = sessionStorage.lastnameSaved;
+} 
+
 function checkLastName(e){
-  let valLastName = e.target.value;
+  if (sessionStorage.lastnameSaved) {
+    valLastName = e.newValue;
+  } 
+  valLastName = e.target.value;
+  sessionStorage.setItem("lastnameSaved", valLastName);
+
   if( valLastName.length < 2 ){
     errorLastName.innerText = "Vous devez saisir au moins 2 caractères";
     errorLastName.style.color = "red";
@@ -123,13 +154,22 @@ e.preventDefault();
 ///////////////////////////////
 
 // check email user
-email.addEventListener('change', checkEmail);
+email.addEventListener('input', checkEmail);
 
 let emailOk = false;
 let valEmail = "";
 
+if (sessionStorage.emailSaved) {
+  email.value = sessionStorage.emailSaved;
+} 
+
 function checkEmail(e){
-  let valEmail = e.target.value;
+  if (sessionStorage.emailSaved) {
+    valEmail = e.newValue;
+  } 
+  valEmail = e.target.value;
+  sessionStorage.setItem("emailSaved", valEmail);
+
     if(valEmail.match(regexMail)){
       errorEmail.innerText = "";
       emailOk = true;
@@ -147,13 +187,22 @@ e.preventDefault();
 ///////////////////////////////////////
 
 // check birthdate user
-birthdate.addEventListener('focusout', checkBirdthDate );
+birthdate.addEventListener('change', checkBirdthDate );
 
 let birdthDateOk = false;
 let valBirthdate = "";
 
+if (sessionStorage.birdthdateSaved) {
+  birthdate.value = sessionStorage.birdthdateSaved;
+} 
+
 function checkBirdthDate(e){
-  let valBirthdate = e.target.value;
+  if (sessionStorage.birdthdateSaved) {
+    valBirthdate = e.newValue;
+  } 
+  valBirthdate = e.target.value;
+  sessionStorage.setItem("birdthdateSaved", valBirthdate);
+
     if (valBirthdate.match(regexBirthdate)){
       errorBirthdate.innerText = "";
       birdthDateOk = true;
@@ -171,17 +220,30 @@ function checkBirdthDate(e){
 /////////////////////////////////////////
 
 // check number of tournaments
-quantityT.addEventListener('change', checkQuantity);
+quantityT.addEventListener('input', checkQuantity);
 
 let valQuantityT = 0;
 
+if (sessionStorage.quantityTSaved) {
+  quantityT.value = parseInt(sessionStorage.quantityTSaved);
+} 
+
 function checkQuantity(e){
-  let val = e.target.value;
+
+  if (sessionStorage.quantityTSaved) {
+    valQuantityT = e.newValue;
+  }
+
+  valQuantityT = e.target.value;
+  sessionStorage.setItem("quantityTSaved", valQuantityT);
+
   if(val != null){
     valQuantityT = parseInt(val);
     return valQuantityT;
   } 
-  alert(valQuantityT);
+  
+  // block form if data are not corrects
+  e.preventDefault();
 }
 
 
@@ -190,17 +252,35 @@ function checkQuantity(e){
 let valCityOk = false;
 let valCity = "";
 
- // check location selected
+// take location selected in sessionStorage
+let valCityChecked = Array.from(document.getElementsByName('location'));
+let val1 = localStorage.getItem('cityChecked');
+
+  for (let i = 0; i < valCityChecked.length; i++) {
+    if (valCityChecked[i].value == val1) {
+      valCityChecked[i].checked = true;
+    }
+  }
+
+ // check location selected to click
 for(let i = 0;i < locationT.length;i++){
 
   locationT[i].addEventListener('click', function(e){
+
     valCity = locationT[i].value;
     valCityOk = true;
+
+    // save location selected in sessionStorage
+    let valCityChecked = document.querySelector('input[name=location]:checked').value;
+    localStorage.setItem("cityChecked", valCityChecked);
+
+    errorLocation.innerText = "";
     alert(valCity)
     return valCity;
   })
 };
 
+ // check location selected in the form validation
 function checkValCity(e){
   if(valCityOk){
     errorLocation.innerText = "";
@@ -216,9 +296,8 @@ function checkValCity(e){
 
 let valOption1ok = true;
 let valOption2ok = false;
-let isFormValid = firstNameOk && lastNameOk && emailOk && birdthDateOk && valCityOk && valOption1ok;
 
-// check checkbox option 1 selected
+// check checkbox option 1 selected to click
 checkboxVal1.onchange = function(e){
   if(checkboxVal1.checked){
     valOption1ok = true;
@@ -232,7 +311,7 @@ checkboxVal1.onchange = function(e){
 }
 
 
-// check checkbox option 2 selected
+// check checkbox option 2 selected to click
 checkboxVal2.onchange = function(e){
   if(checkboxVal2.checked){
     valOption2ok = true;
@@ -240,60 +319,71 @@ checkboxVal2.onchange = function(e){
   } 
 }
 
-function checkConditions(e){
-
+// check if checkox 1 selected to the form validation 
+function checkCondition1(e){
+  if(checkboxVal1.checked){
+    errorCheckbox.innerText = "";
+    return true;
+  } else {
+    errorCheckbox.innerText = "Veuillez valider les conditions d'utilisation";
+    errorCheckbox.style.color = "red";
+    errorCheckbox.style.fontSize = "18px";
+  }
 }
 
 //////////////////////////////////////////
-  // validation formulaire
 
+  // validation form
+  let isFormValid = firstNameOk && lastNameOk && emailOk && birdthDateOk && valCityOk && valOption1ok;
 
-function disableSubmit(disabled) {
-  const buttonSubmit = document.getElementById('btn-submit');
-  if (disabled) {
-    buttonSubmit.setAttribute("disabled", true);
-  } else {
-    buttonSubmit.removeAttribute("disabled");
-  }
-}
+// function disableSubmit(disabled) {
+//   const buttonSubmit = document.getElementById('btn-submit');
+//   if (disabled) {
+//     buttonSubmit.setAttribute("disabled", true);
+//   } else {
+//     buttonSubmit.removeAttribute("disabled");
+//   }
+// }
 
-form.addEventListener("change", (e) => {
-  console.log(e, isFormValid);
-  if(isFormValid){
-    disableSubmit(false);
-  } else {
-
-    disableSubmit(true);
-  }
-})
+// form.addEventListener("submit", (e) => {
+// // form.onload = (e) => {
+//   // console.log(e, isFormValid);
+//   if(isFormValid){
+//     alert("formulaire sauvegardé");
+//     disableSubmit(false);
+//   } else {
+//     disableSubmit(true);
+//     e.preventDefault();
+//   }
+// })
 
 
 const buttonSubmit = document.getElementById('btn-submit');
 buttonSubmit.addEventListener('click', formValidation);
 
   function formValidation(e){
+    
+  checkFirstName();
+  checkLastName();
+  checkEmail();
+  checkBirdthDate();
+  checkQuantity();
+  checkValCity(locationT[i].value);
+  checkCondition1(checkboxVal1.value);
 
-  checkFirstName(e);
-  checkLastName(e);
-  checkEmail(e);
-  checkBirdthDate(e);
-  checkQuantity(e);
-  checkValCity(e);
-
-
-
-  if(firstNameOk && lastNameOk && emailOk && birdthDateOk && valCityOk && valOption1ok){
+  if(isFormValid){
     alert("le formulaire est valide");
     
     modalbg.style.display = "none";
     form.submit();
     form.reset();
+    sessionStorage.clear();
 
   } else {
     alert("le formulaire est incomplet, veuillez le remplir");
-    e.preventDefault();
+    // e.preventDefault();
   }
-  // e.preventDefault();
+  e.preventDefault();
   };
 
   ////////////////////////////////////////////////////////////////////////////////////////
